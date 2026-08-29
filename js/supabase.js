55387c5d4625dce024398c686a462dd567d16ba5
adham05042025-lang <adham05042025@gmail.com>
2026-08-29 04:02:44 +0300

    Fix Supabase publishable API key

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = 'https://cvhgwtjfpwnaergxjldl.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_Lo0SsmalFrf-srccTMig_q4ryBu2K';
+const SUPABASE_ANON_KEY = 'sb_publishable_Lo0SsmalFrf-fssrccTMig_q4ryBu2K';
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
